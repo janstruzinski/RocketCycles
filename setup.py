bc21ca7f83be4aa3a8dfa383ef17574680914644
jstruzinski <jashorpl@gmail.com>
2024-07-26 10:53:11 +0200

    Update readme and setup

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
 setup(name='rocketcycles',
       version='0.1',
       description='Python package for analysis and sizing of staged combustion rocket engines',
@@ -7,6 +12,10 @@ setup(name='rocketcycles',
       author='Jan Struzinski',
       license='MIT',
       packages=['rocketcycles'],
-      install_requires=['rocketcea', 'scipy', 'numpy', 'nasaPoly', 'pyfluids'],
+      install_requires=['rocketcea', 'scipy', 'numpy', 'pyfluids'],
       keywords='staged combustion liquid rocket engine full flow',
-      zip_safe=False)
+      dependency_links=['https://github.com/ptgodart/nasaPoly.git'],
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/x-rst'
+      )
