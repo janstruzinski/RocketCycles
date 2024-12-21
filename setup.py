08c34f63b277e436c5820a6e062f3c1e1ba4d0c0
janstruzinski <jashorpl@gmail.com>
2024-12-22 00:49:35 +0100

    Changed version to 1.0.1 and added required packages versions

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='rocketcycles',
-      version='1.0',
+      version='1.0.1',
       description='Python package for analysis and sizing of staged combustion rocket engines',
       url='https://github.com/janstruzinski/RocketCycles.git',
       author='Jan Struzinski',
       license='MIT',
       packages=['rocketcycles'],
-      install_requires=['rocketcea', 'scipy', 'numpy', 'pyfluids'],
+      install_requires=['rocketcea>=1.2.1', 'scipy>=1.14.1', 'numpy>=2.1.1', 'pyfluids>=2.6.0'],
       keywords='staged combustion liquid rocket engine cycle full flow oxidizer rich cea rocketcea',
       dependency_links=['https://github.com/ptgodart/nasaPoly.git'],
       zip_safe=False,
